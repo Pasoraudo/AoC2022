d8322af14157e77d63e0b4237bcf3ae7f32eef98
Pasoraudo <jandrollobell@gmail.com>
2022-12-01 14:30:12 +0100

    p

--- day2.py
+++
@@ -1,9 +0,0 @@
-with open('./data/day1.txt', 'r') as f:
-    data = f.read().split('\n\n')
-calories = []
-for line in data:
-    num = sum([int(i) for i in line.split()])
-    calories.append(num)
-calories = sorted(calories, reverse=True)
-print('Problema 1: ', calories[0])
-print('Problema 2: ', sum(calories[:3]))
